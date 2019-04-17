4f2935451492f0db2a4b3c4257629460e301b21a
chirayu brijwani <chirayubrijwani203@gmail.com>
2019-04-17 18:24:28 +0530

    'init'

--- startup/db.js
+++ startup/db.js
@@ -5,13 +5,7 @@ module.exports = function() {
   mongoose.connect('mongodb+srv://chirayu:asdf12345@cluster0-fy3uu.mongodb.net/test?retryWrites=true')
     .then(() => winston.info('Connected to MongoDB...'));
 }
+
+// Use below settings for local 
 // mongodb://localhost/vidly
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://chirayu:asdf12345@cluster0-fy3uu.mongodb.net/test?retryWrites=true";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("Vidly").collection("Movie Store");
-//   // perform actions on the collection object
-//   client.close();
-// });
