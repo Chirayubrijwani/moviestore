1d422e1bfffed1e57e8cffd9218f566426bcbd08
chirayu brijwani <chirayubrijwani203@gmail.com>
2019-04-17 18:02:59 +0530

    'mongo'

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
+// const MongoClient = require('mongodb').MongoClient;
+
+// const uri = "mongodb+srv://chirayu:asdf12345@cluster0-fy3uu.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//     if(err) {
+//         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+//    }
+//    console.log('Connected...');
+
+//   const collection = client.db("Vidly").collection("Movie Store");
+  
+//     collection.find({}).toArray((error, result) => {
+//         if(error) {
+//             // return response.status(500).send(error);
+//             console.log("hogaya");
+//         }
+//         // response.send(result);
+//         console.log("kya",result);
+//     }); 
+
+  
+
+//   client.close();
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
--- startup/db.js
+++ startup/db.js
@@ -5,3 +5,13 @@ module.exports = function() {
   mongoose.connect('mongodb://localhost/vidly')
     .then(() => winston.info('Connected to MongoDB...'));
 }
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://chirayu:asdf12345@cluster0-fy3uu.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("Vidly").collection("Movie Store");
+//   // perform actions on the collection object
+//   client.close();
+// });
